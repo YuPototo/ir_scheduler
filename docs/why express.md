87fd7b3d73283a50e131aadaa35b46bf577fe686
Yu Qin <sirius.qinyu@gmail.com>
2023-01-06 11:50:33 +0800

    Add express

---
+++ docs/why express.md
@@ -0,0 +1,5 @@
+# 为什么使用 Express？
+
+为了让 Digital Ocean 的 App Platform 可以识别出这是一个 app 应用并成功部署。
+
+这是一个 hack。
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
 import deleteAccountsJob from "./jobs/deleteAccounts";
 import updatePriceAndRank from "./jobs/updatePrice";
 
-async function main() {
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server is working");
+});
+
+app.listen(port, () => {
     deleteAccountsJob();
     updatePriceAndRank();
-}
-
-main();
+    console.log(`⚡️[server]: Server is running at port: ${port}`);
+});
